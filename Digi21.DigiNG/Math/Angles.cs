fd6de5b62811bb143f05286a5ae828d5c26e3c7b
José Ángel <joseangelmt@outlook.com>
2021-04-04 11:15:59 +0200

    Added support to extension methods

--- Digi21.DigiNG/Math/Angles.cs
+++ Digi21.DigiNG/Math/Angles.cs
@@ -3,19 +3,19 @@ namespace Digi21.Math
 {
     public static class Angles
     {
-        public static double AzimuthToTrigonometric(double radians) { throw null; }
+        public static double AzimuthToTrigonometric(this double radians) { throw null; }
         public static void DMSToSexagesimal(int degrees, int minutes, double seconds, bool east, out double sexagesimal) { throw null; }
-        public static double GradianToRadian(double gradian) { throw null; }
-        public static double GradianToSexagesimal(double radian) { throw null; }
-        public static void MatrixToRotations(double[,] eulerMatrix, out double omega, out double phi, out double kappa) { throw null; }
-        public static double NormalizeAngle(double radians) { throw null; }
-        public static double RadianToGradian(double radian) { throw null; }
-        public static double RadianToSexagesimal(double radian) { throw null; }
+        public static double GradianToRadian(this double gradian) { throw null; }
+        public static double GradianToSexagesimal(this double radian) { throw null; }
+        public static void MatrixToRotations(this double[,] eulerMatrix, out double omega, out double phi, out double kappa) { throw null; }
+        public static double NormalizeAngle(this double radians) { throw null; }
+        public static double RadianToGradian(this double radian) { throw null; }
+        public static double RadianToSexagesimal(this double radian) { throw null; }
         public static void RotationsToMatrix(double omega, double phi, double kappa, out double[,] eulerMatrix) { throw null; }
-        public static void SexagesimalToDMS(double sexagesimal, out int degrees, out int minutes, out double seconds, out bool east) { throw null; }
-        public static double SexagesimalToGradian(double sexagesimal) { throw null; }
-        public static double SexagesimalToRadian(double sexagesimal) { throw null; }
-        public static double TrigonometricToAzimuth(double radians) { throw null; }
+        public static void SexagesimalToDMS(this double sexagesimal, out int degrees, out int minutes, out double seconds, out bool east) { throw null; }
+        public static double SexagesimalToGradian(this double sexagesimal) { throw null; }
+        public static double SexagesimalToRadian(this double sexagesimal) { throw null; }
+        public static double TrigonometricToAzimuth(this double radians) { throw null; }
     }
 
 
