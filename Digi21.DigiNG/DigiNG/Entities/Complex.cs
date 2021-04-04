00b08b1f36e35351e046ec190c32586713beb5e0
José Ángel <joseangelmt@outlook.com>
2021-04-04 19:02:16 +0200

    Translated name of parameter to english

--- Digi21.DigiNG/DigiNG/Entities/Complex.cs
+++ Digi21.DigiNG/DigiNG/Entities/Complex.cs
@@ -6,7 +6,7 @@ namespace Digi21.DigiNG.Entities
     public class Complex : ReadOnlyComplex
     {
         public Complex(Code code) => throw null;
-        public Complex(IEnumerable<Code> codigos) => throw null;
+        public Complex(IEnumerable<Code> codes) => throw null;
 
         public new IList<Entity> Entities => throw null;
     }
