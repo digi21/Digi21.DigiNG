7e30636f87652e683cff985bf6eb7f04ea172277
José Ángel <joseangelmt@outlook.com>
2021-04-04 12:41:30 +0200

    ReadOnlyPolygon.Holes now is of type IEnumerable<ReadOnlyLine>

--- Digi21.DigiNG/DigiNG/Entities/ReadOnlyPolygon.cs
+++ Digi21.DigiNG/DigiNG/Entities/ReadOnlyPolygon.cs
@@ -15,19 +15,8 @@ namespace Digi21.DigiNG.Entities
         public override string ToString() => throw null;
         public IEnumerable<Polygon> Trim(ReadOnlyLine limit, bool coordinateZFromThisEntity) => throw null;
         public double Area => throw null;
-        public ReadOnlyLineReadOnlyCollection Holes => throw null;
+        public IEnumerable<ReadOnlyLine> Holes => throw null;
         public Point3DReadOnlyCollection Points => throw null;
         public Point3D InteriorPoint => throw null;
-
-        public class ReadOnlyLineReadOnlyCollection : IReadOnlyList<ReadOnlyLine>
-        {
-            public bool Contains(ReadOnlyLine entity) => throw null;
-            public void CopyTo(ReadOnlyLine[] array, int arrayIndex) => throw null;
-            public IEnumerator<ReadOnlyLine> GetEnumerator() => throw null;
-            IEnumerator IEnumerable.GetEnumerator() => throw null;
-            public int IndexOf(ReadOnlyLine item) => throw null;
-            public ReadOnlyLine this[int index] { get => throw null; }
-            public int Count => throw null;
-        }
     }
 }
