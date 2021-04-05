3f1a87afa3dec5d82f43086782424c96c5da7d3c
José Ángel <joseangelmt@outlook.com>
2021-04-05 22:47:38 +0200

    IReadOnlyDrawingFile now hinerits from IEnumerable. IReadOnlyDrawingFile.get_DatabaseAttributes() now is GetDatabaseAttributes

--- Digi21.DigiNG/DigiNG/IO/IReadOnlyDrawingFile.cs
+++ Digi21.DigiNG/DigiNG/IO/IReadOnlyDrawingFile.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 
 namespace Digi21.DigiNG.IO
 {
-    public interface IReadOnlyDrawingFile : IList<Entity>
+    public interface IReadOnlyDrawingFile : IEnumerable<Entity>
     {
-        // Warning: Properties with arguments are not supported in C#. Getter of a DatabaseAttributes property was decompiled as a method.
-        IDictionary<string, IDictionary<string, object>> get_DatabaseAttributes(Entity entity);
+        IDictionary<string, IDictionary<string, object>> GetDatabaseAttributes(Entity entity);
     }
 }
