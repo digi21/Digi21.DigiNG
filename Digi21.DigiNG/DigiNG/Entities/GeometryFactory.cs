9fc6262b7151b89e5eee774ebf413dfd60f7bc7d
José Ángel <joseangelmt@outlook.com>
2021-04-28 13:23:19 +0200

    Added type GeometryFactory. digi21/TareasDigi3D#347

---
+++ Digi21.DigiNG/DigiNG/Entities/GeometryFactory.cs
@@ -0,0 +1,8 @@
+﻿namespace Digi21.DigiNG.Entities
+{
+    public static class GeometryFactory 
+    {
+        public static Line CreateParallel(this ReadOnlyLine line, double distance) => throw null;
+        public static Line CreateParallel(this ReadOnlyLine line, double distanceXY, double distanceZ) => throw null;
+    }
+}
