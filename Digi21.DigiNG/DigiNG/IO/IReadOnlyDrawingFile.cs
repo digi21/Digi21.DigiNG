f125f171dbb62cabf5c1f54a388e7f297f70b615
José Ángel <joseangelmt@outlook.com>
2021-12-10 12:45:28 +0100

    IReadOnlyDrawingFile now implements IWindow3D. digi21/TareasDigi3D#432

--- Digi21.DigiNG/DigiNG/IO/IReadOnlyDrawingFile.cs
+++ Digi21.DigiNG/DigiNG/IO/IReadOnlyDrawingFile.cs
@@ -1,9 +1,12 @@
 ﻿using Digi21.DigiNG.Entities;
 using System.Collections.Generic;
+using Digi21.Math;
 
 namespace Digi21.DigiNG.IO
 {
-    public interface IReadOnlyDrawingFile : IEnumerable<Entity>
+    public interface IReadOnlyDrawingFile 
+        : IEnumerable<Entity>
+        , IWindow3D
     {
         string Path { get; }
         string ConnectionString { get; }
