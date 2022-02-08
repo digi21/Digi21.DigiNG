3e7fd3b8f55e30037f336f767dea2c13b9da77a2
José Ángel <joseangelmt@outlook.com>
2022-02-08 11:16:30 +0100

    Downgraded version

--- Digi21.DigiNG/Properties/AssemblyInfo.cs
+++ Digi21.DigiNG/Properties/AssemblyInfo.cs
@@ -34,4 +34,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("22.2.0.0")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
