1e2dd2c84d54616bf044d4048f883a0ab05e7b7a
José Ángel <joseangelmt@outlook.com>
2023-12-12 18:33:53 +0100

    Actualizado a .NET 8.0

--- Digi21.DigiNG/Properties/AssemblyInfo.cs
+++ Digi21.DigiNG/Properties/AssemblyInfo.cs
@@ -6,12 +6,6 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: ReferenceAssembly]
-[assembly: AssemblyTitle("Digi21.DigiNG")]
-[assembly: AssemblyDescription("Implements DigiNG types of Digi3D.NET")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Dreaming With Objects S.L.")]
-[assembly: AssemblyProduct("Digi3D.NET")]
-[assembly: AssemblyCopyright("Copyright © José Ángel Martínez Torres 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -22,16 +16,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("01e76c8f-987c-4631-9e32-29c1d5c9e72f")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("22.4.0.0")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
