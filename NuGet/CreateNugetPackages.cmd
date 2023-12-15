8ded64d89e3e858d49ad80f4bfcf2c799b515d5d
José Ángel <joseangelmt@outlook.com>
2023-12-15 09:03:13 +0100

    El paquete NuGet ahora proporciona referencias y no librerías

--- NuGet/CreateNugetPackages.cmd
+++ NuGet/CreateNugetPackages.cmd
@@ -1,4 +1,4 @@
 @echo off
 nuget.exe pack Digi21.DigiNG.nuspec
-nuget.exe sign Digi21.DigiNG.22.4.0.nupkg -CertificateSubjectName "DREAMING WITH OBJECTS S.L." -TimeStamper http://timestamp.comodoca.com?td=sha256.
+nuget.exe sign *.nupkg -CertificatePath "C:\Users\josea\OneDrive\certificados\certificadosslparafirmarnuget.pfx" -TimeStamper http://timestamp.comodoca.com?td=sha256.
 
